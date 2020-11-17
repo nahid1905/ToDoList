2ecd22f64ab706ff46fe84931490d9c7db369845
Nahid A. Shukurlu <ShukurluN@kapitalho.az>
2020-11-17 17:52:01 +0400

    Dynamic id works. Input validation added

--- js/main.js
+++ js/main.js
@@ -4,17 +4,16 @@ var toDo = document.querySelector('.to-do');
 var id = 0
 
 function add(){
-    id++;
-    event.preventDefault();
-    // var div = toDo.appendChild('div');
-    // div.setAttribute('div', id);
-    let div = document.createElement("div");
-    div = document.body.appendChild(div);
-    div.setAttribute('class', 'item');
-    div.innerText = input.value;
-    toDo.appendChild(div);
-    // console.log(document.body.appendChild(div));
-    // console.log(input.value);
-    input.value = '';
+    if(input.value != ''){
+        id++;
+        event.preventDefault();
+        let div = document.createElement("div");
+        div = document.body.appendChild(div);
+        div.setAttribute('class', 'item');
+        div.setAttribute('id', id);
+        div.textContent = input.value;
+        toDo.appendChild(div);
+        input.value = '';
+    }
 }
 addButton.addEventListener('click', add);
