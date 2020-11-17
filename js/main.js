ef757887b6c97f65562e2e9ef4947acf13984069
Master <Master@MASTER-PC>
2020-11-18 00:31:32 +0400

    Complete() function ready

--- js/main.js
+++ js/main.js
@@ -1,19 +1,39 @@
 var input = document.querySelector('#toDoInput');
-var addButton = document.querySelector('#add');
-var toDo = document.querySelector('.to-do');
+var addBtn = document.querySelector('#add');
+var toDo = document.querySelector('.to-do-list');
+var completedList = document.querySelector('.completed-list');
+var toDoItem = document.querySelector('.item');
+var deleteBtn = document.querySelector('.fa-trash');
+let div = document.createElement("div");
+
 var id = 0
 
+
+function createToDoItem(){
+    id++;
+    div = document.body.appendChild(div);
+    div.setAttribute('class', 'item');
+    div.setAttribute('id', id);
+    toDo.appendChild(div);
+    div.textContent = input.value;
+}
 function add(){
+    event.preventDefault();
     if(input.value != ''){
-        id++;
-        event.preventDefault();
-        let div = document.createElement("div");
-        div = document.body.appendChild(div);
-        div.setAttribute('class', 'item');
-        div.setAttribute('id', id);
-        div.textContent = input.value;
-        toDo.appendChild(div);
+        createToDoItem();
         input.value = '';
     }
 }
-addButton.addEventListener('click', add);
+function deleteItemFromToDo(){
+    let itemForDelete = deleteBtn.parentElement.parentElement;
+    itemForDelete.remove();
+}
+function complete(){
+    deleteItemFromToDo();
+    completedList.appendChild(div);
+    div.style.textDecoration = 'line-through';
+    div.style.backgroundColor = 'rgb(93 138 35)'
+}
+
+deleteBtn.addEventListener('click', deleteItemFromToDo);
+addBtn.addEventListener('click', add);
