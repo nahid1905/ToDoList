f16a6f264cee1b40e7c05cf41bf76b76090086d4
Master <Master@MASTER-PC>
2020-11-19 01:08:19 +0400

    all functionalities work except drag and drop

--- js/main.js
+++ js/main.js
@@ -2,37 +2,47 @@ var input = document.querySelector('#toDoInput');
 var addBtn = document.querySelector('#add');
 var toDo = document.querySelector('.to-do-list');
 var completedList = document.querySelector('.completed-list');
-var buttons = document.createElement('div');
-var editBtn = document.createElement('i');
-var trashBtn = document.createElement('i');
-var checkBox = document.createElement('INPUT');
+var dropZoneCompleted = document.querySelector('.completed-list');
+var dropZoneToDo = document.querySelector('.to-do-list');
+var elementDragged = null;
+var allToDoItems = document.querySelectorAll('.to-do-list .item');
 
 let id = 0
 
-
 // create item template
 function createToDoItem(itemId){
-    var item = document.createElement('div');
+    let item = document.createElement('div');
+    let checkBox = document.createElement('input');
+    let trashBtn = document.createElement('i');
+    let editBtn = document.createElement('i');
+    let buttons = document.createElement('div');
     
-    let itemDiv = document.body.appendChild(item);
-    itemDiv.setAttribute('class', 'item');
-    itemDiv.setAttribute('id', itemId);
+    item = document.body.appendChild(item);
+    item.setAttribute('class', 'item');
+    item.setAttribute('id', itemId);
     item.setAttribute('draggable', true);
-    let checkBoxIcon = itemDiv.appendChild(checkBox);
-    checkBoxIcon.setAttribute('type', 'checkbox');
-    checkBoxIcon.setAttribute('id', itemId);
-    itemDiv.textContent = input.value;
-    btnsDiv = itemDiv.appendChild(buttons);
-    btnsDiv.setAttribute('class', 'buttons');
-    let editIcon = btnsDiv.appendChild(editBtn);  
-    editIcon.setAttribute('class', 'fa fa-edit');
-    editIcon.setAttribute('id', itemId);
-    let trashIcon = btnsDiv.appendChild(trashBtn);
-    trashIcon.setAttribute('class', 'fa fa-trash');
-    trashIcon.setAttribute('id', itemId);
-    itemDiv.insertBefore(checkBoxIcon, itemDiv.firstChild);
-    toDo.appendChild(itemDiv);
+    
+    checkBox = item.appendChild(checkBox);
+    checkBox.setAttribute('type', 'checkbox');
+    checkBox.setAttribute('id', itemId);
+    item.textContent = input.value;
+    item.insertBefore(checkBox, item.firstChild);
+    checkBox.addEventListener('click', () => checkBox.checked ? complete(itemId) : uncomplete(itemId))
+
+    buttons = item.appendChild(buttons);
+    buttons.setAttribute('class', 'buttons');
+
+    editBtn = buttons.appendChild(editBtn);  
+    editBtn.setAttribute('class', 'fa fa-edit');
+    editBtn.setAttribute('id', itemId);
+
+    trashBtn = buttons.appendChild(trashBtn);
+    trashBtn.setAttribute('class', 'fa fa-trash');
+    trashBtn.setAttribute('id', itemId);
+    trashBtn.addEventListener('click', deleteItem);
+    toDo.appendChild(item);
 }
+
 function add(){
     event.preventDefault();
     if(input.value != ''){
@@ -41,50 +51,34 @@ function add(){
         input.value = '';
     }
 }
-function deleteItemFromToDo(itemId){
-    let itemForDelete = document.getElementById(itemId);
+function deleteItem(event){
+    let itemForDelete = document.getElementById(event.target.id);
     itemForDelete.remove();
 }
-function complete(itemId){
-    let itemForComplete = document.getElementById(itemId);
+// completed tasks add to CompletedList 
+function complete(id){
+    let itemForComplete = document.getElementById(id);
     itemForComplete.classList.add('completed');
     completedList.appendChild(itemForComplete);
 }
-function uncomplete(itemId){
-    let itemForUncomplete = document.getElementById(itemId);
+// unchecked tasks back to todolist from completedlist
+function uncomplete(id){
+    let itemForUncomplete = document.getElementById(id);
     itemForUncomplete.classList.remove('completed');
     toDo.appendChild(itemForUncomplete);
 }
-
-checkBox.addEventListener('click', function(ev){
-    let id = ev.target.id;
-    if(checkBox.checked){
-        complete(id);
-    }
-    else{
-        uncomplete(id);
-    }
-});
-trashBtn.addEventListener('click', function(ev){
-    deleteItemFromToDo(ev.target.id);
-    console.log(ev.target.id);
-});
 addBtn.addEventListener('click', add);
 
 
-var dropZoneCompleted = document.querySelector('.completed-list');
-var dropZoneToDo = document.querySelector('.to-do-list');
-var dragElements = document.querySelectorAll('.to-do-list .item');
-var elementDragged = null;
+// drag and drop
+for (var i = 0; i < allToDoItems.length; i++) {
 
-for (var i = 0; i < dragElements.length; i++) {
-
-    dragElements[i].addEventListener('dragstart', function(e) {
+    allToDoItems[i].addEventListener('dragstart', function(e) {
         e.dataTransfer.effectAllowed = 'move';
         e.dataTransfer.setData('text', this.innerHTML);
         elementDragged = this;
     });
-    dragElements[i].addEventListener('dragend', function(e) {
+    allToDoItems[i].addEventListener('dragend', function(e) {
         elementDragged = null;
     });
 };
